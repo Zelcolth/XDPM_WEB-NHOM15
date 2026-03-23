7bb26e28736bb9cd8b59903a450093519789aefc
ledoduyphuc <saochanel0938@gmail.com>
2026-03-23 21:17:55 +0700

    update homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OrderItem />} /> //Để tạm thời, sau này sẽ đổi thành trang chủ
+        <Route path="/" element={<Home />} /> //Để tạm thời, sau này sẽ đổi thành trang chủ
         <Route path="/checkout" element={<Checkout />} /> 
-        <Route path="/home" element={<Home />} />
+        <Route path="/order" element={<OrderItem />} />
       </Routes>
     </BrowserRouter>
   );
