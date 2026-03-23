79900e6ac02b211aa0ee0e127da8532c6d32acb8
ledoduyphuc <saochanel0938@gmail.com>
2026-03-23 21:04:09 +0700

    Hoàn thành giao diện trang Home + navbar + slider + login/register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrderItem from './pages/OrderItem';
 import Checkout from './pages/Checkout';
+import Home from './pages/Home';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<OrderItem />} /> //Để tạm thời, sau này sẽ đổi thành trang chủ
         <Route path="/checkout" element={<Checkout />} /> 
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
