563ab5feed4e0ed69bdfbfec16622f45e3674940
huy an <huyan@huyan-MacBook-Air.local>
2026-03-24 23:07:13 +0700

    Cập nhật frontend: auth và tích hợp API

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -7,4 +7,12 @@ const axiosClient = axios.create({
     },
 });
 
+export function setAuthToken(token) {
+    if (token) {
+        axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        delete axiosClient.defaults.headers.common['Authorization'];
+    }
+}
+
 export default axiosClient;
